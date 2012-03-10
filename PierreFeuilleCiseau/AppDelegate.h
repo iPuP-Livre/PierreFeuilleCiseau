12512a460e47b9450b260770c4667b80df8a274d
ipodishima <marian.paul2@gmail.com>
2012-03-10 15:15:32 +0100

    Initial Commit

---
+++ PierreFeuilleCiseau/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PierreFeuilleCiseau
+//
+//  Created by Marian PAUL on 10/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
